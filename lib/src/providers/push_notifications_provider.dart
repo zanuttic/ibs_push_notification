cd2d22e9ede8f58b231aa96a5aeb5d3ca75f4c97
Carlos Zanutti <carlos.zanutti@gmail.com>
2020-12-12 03:44:54 -0300

    Codigo y configuracion IOS lista

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationsProvider {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   final _mensajesStreamController = StreamController<String>.broadcast();
-  Stream<String> get mensajesStream=> _mensajesStreamController.stream;
+  Stream<String> get mensajesStream => _mensajesStreamController.stream;
 
-  static Future<dynamic> onBackgroundMessage(Map<String, dynamic> message) async {
+  static Future<dynamic> onBackgroundMessage(
+      Map<String, dynamic> message) async {
     if (message.containsKey('data')) {
       final dynamic data = message['data'];
     }
@@ -16,22 +18,25 @@ class PushNotificationsProvider {
       final dynamic notification = message['notification'];
     }
   }
-  initNotification() async{
+
+  initNotification() async {
     await _firebaseMessaging.requestNotificationPermissions();
-    final token= await _firebaseMessaging.getToken();
+    final token = await _firebaseMessaging.getToken();
 
     print('Token: $token');
 
-  _firebaseMessaging.configure(
+    _firebaseMessaging.configure(
         onMessage: onMessage,
-        onBackgroundMessage: onBackgroundMessage,
+        onBackgroundMessage: Platform.isIOS
+            ? null
+            : PushNotificationsProvider.onBackgroundMessage,
         onLaunch: onLaunch,
-        onResume: onResume
-      );
+        onResume: onResume);
   }
-   Future<dynamic> onMessage(Map<String, dynamic> message) async {
-    final titulo= message['notification']['title'];
-    final body= message['notification']['body'];
+
+  Future<dynamic> onMessage(Map<String, dynamic> message) async {
+    final titulo = message['notification']['title'];
+    final body = message['notification']['body'];
     _mensajesStreamController.sink.add(body);
 
     print('========== onMessage===========');
@@ -39,25 +44,34 @@ class PushNotificationsProvider {
     print('titulo: $titulo');
     print('body: $body');
   }
-   Future<dynamic> onLaunch(Map<String, dynamic> message) async {
-    
-     final mensaje= message['data']['mensaje'];
-    _mensajesStreamController.sink.add(mensaje);
+
+  Future<dynamic> onLaunch(Map<String, dynamic> message) async {
+    String mensaje = 'Sin-datos';
+    if (Platform.isAndroid) {
+      mensaje = message['data']['mensaje'];
+      _mensajesStreamController.sink.add(mensaje);
+    } else {
+      mensaje = message['mensaje'] ?? 'Sin-Datos';
+    }
 
     print('========== onResume===========');
     print('mensage: $mensaje');
   }
-   Future<dynamic> onResume(Map<String, dynamic> message) async {
-    
-    
-    final mensaje= message['data']['mensaje'];
-    _mensajesStreamController.sink.add(mensaje);
+
+  Future<dynamic> onResume(Map<String, dynamic> message) async {
+    String mensaje = 'Sin-datos';
+    if (Platform.isAndroid) {
+      mensaje = message['data']['mensaje'];
+      _mensajesStreamController.sink.add(mensaje);
+    } else {
+      mensaje = message['mensaje'] ?? 'Sin-Datos';
+    }
 
     print('========== onResume===========');
     print('mensage: $mensaje');
   }
-  dispose(){
 
+  dispose() {
     _mensajesStreamController?.close();
   }
 }
